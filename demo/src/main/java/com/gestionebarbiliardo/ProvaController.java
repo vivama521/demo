3f7567d8e76ebb855583540da2546818936f992d
sviluppo <sviluppo@sviluppo-VirtualBox>
2018-07-20 01:03:13 +0200

    mezza configurazione

--- demo/src/main/java/com/gestionebarbiliardo/ProvaController.java
+++ demo/src/main/java/com/gestionebarbiliardo/ProvaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProvaController {
 
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	public String hello() {
 		return "hello";
 	}
