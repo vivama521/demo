638d2bb2b1213b972e6c549444c7a1fbb62d3b75
Marzio Tullio <m.tullio@solvingteam.it>
2018-07-20 17:01:25 +0200

    commit project gay

---
+++ GestioneBiliardi/src/test/java/com/gestionebiliardi/GestioneBiliardiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gestionebiliardi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestioneBiliardiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
