7e69e6aed78dcdd1d1d77deebb36b55c14295004
Marzio Tullio <m.tullio@solvingteam.it>
2018-07-20 15:18:26 +0200

    ottimizzazione risultati test

--- GestioneBiliardi/src/main/java/com/gestionebiliardi/controller/BiliardiController.java
+++ GestioneBiliardi/src/main/java/com/gestionebiliardi/controller/BiliardiController.java
@@ -1,11 +1,17 @@
 package com.gestionebiliardi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gestionebiliardi.service.ResultGay;
+
 @Controller
 public class BiliardiController {
+	
+	@Autowired
+	private ResultGay resultGay;
 	
 	@RequestMapping("/")
 	public ModelAndView hello(){
@@ -14,4 +20,10 @@ public class BiliardiController {
 		
 	}
 
+	@RequestMapping("/linkGay")
+	public ModelAndView gayResponse(){
+		String name = resultGay.getGayOfTheMoment();
+		return new ModelAndView("resultGay","name", name);
+		
+	}
 }
---
+++ GestioneBiliardi/src/main/java/com/gestionebiliardi/service/ResultGay.java
@@ -0,0 +1,27 @@
+package com.gestionebiliardi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResultGay {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGayOfTheMoment(){
+		
+		int result = (int) (Math.random()*10);
+		if(result <= 5){
+			return "Valerio";
+		}else{
+			return "Vittorio";
+		}
+	}
+}
