c64dc306abdd10e7912c28ce717a0dc064a2f38d
Marzio Tullio <m.tullio@solvingteam.it>
2018-07-23 18:43:57 +0200

    config file log <contextPath>/logs/gestioneBiliardiLog.log

--- GestioneBiliardi/src/main/java/com/gestionebiliardi/controller/BiliardiController.java
+++ GestioneBiliardi/src/main/java/com/gestionebiliardi/controller/BiliardiController.java
@@ -1,5 +1,7 @@
 package com.gestionebiliardi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,15 @@ import com.gestionebiliardi.serviceimpl.ResultGay;
 @Controller
 public class BiliardiController {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private ResultGay resultGay;
 	
 	@RequestMapping("/")
 	public ModelAndView hello(){
 		String message = "Chi è piu' gay tra Valerio e Vittorio?";
+		logger.debug("Message: " + message);
 		return new ModelAndView("hello","message", message);
 		
 	}
